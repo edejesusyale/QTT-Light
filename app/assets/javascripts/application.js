0b3941f143e95a7e16e69747f63d951bd90317eb
edejesusyale <eric.dejesus@yale.edu>
2018-07-30 16:00:24 -0400

    Added nested table to visually compare Solr to oracle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,28 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require bootstrap
+//= require_tree .
+
+$(document).ready(function () {
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            $('#scroll_to_top').fadeIn()
+        } else {
+            $('#scroll_to_top').fadeOut()
+        }
+    }
+
+});
+
+function topFunction() {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 800);
+    //document.body.scrollTop = 0;
+    //document.documentElement.scrollTop = 0;
+}
